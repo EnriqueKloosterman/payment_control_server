20a2b98303ef7dd91327d61c4afbe282e4c7533b
unknown <kloostermanen@gmail.com>
2026-02-24 19:53:39 -0300

    logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -13,15 +13,9 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: path.join(__dirname, '../../logs/error.log'), level: 'error' }),
     new winston.transports.File({ filename: path.join(__dirname, '../../logs/combined.log') }),
+    new winston.transports.Console({ format: winston.format.simple() })
   ],
 });
 
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
 
 module.exports = logger;
